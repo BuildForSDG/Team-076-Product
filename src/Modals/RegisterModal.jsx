4a5712736d1e83262c5e2ca54bdc848027ed5079
ojknation <ojknation@gmail.com>
2020-05-16 00:19:44 +0100

    feat: Put validation rules on forms
    #Fix001

--- src/Modals/RegisterModal.jsx
+++ src/Modals/RegisterModal.jsx
@@ -13,10 +13,26 @@ import {
   Input,
   Button,
   Select,
+  FormErrorMessage,
 } from "@chakra-ui/core";
 import styles from "./Modals.module.css";
+import { useForm } from "react-hook-form";
 
 const RegisterModal = () => {
+  const { register, handleSubmit, formState, errors } = useForm();
+
+  const validateName = (value) => {
+    let error;
+    if (!value) {
+      error = "You cannot leave this field empty";
+    }
+    return error || true;
+  };
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data, null, 2));
+  };
+
   const initialRef = useRef();
   const dispatch = useDispatch();
   const closemodal = () => {
@@ -31,50 +47,82 @@ const RegisterModal = () => {
     >
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader pt={5}>Create a free account</ModalHeader>
-        <ModalBody pb={6}>
-          <FormControl isRequired>
-            <FormLabel>Name</FormLabel>
-            <Input
-              ref={initialRef}
-              width="90%"
-              placeholder="Enter your name here"
-            />
-          </FormControl>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <ModalHeader pt={5}>Create a free account</ModalHeader>
+          <ModalBody pb={6}>
+            <FormControl isInvalid={errors.name}>
+              <FormLabel>Name</FormLabel>
+              <Input
+                name="name"
+                ref={(initialRef, register({ validate: validateName }))}
+                width="90%"
+                placeholder="Enter your name here"
+              />
+              <FormErrorMessage>
+                {errors.name && errors.name.message}
+              </FormErrorMessage>
+            </FormControl>
 
-          <FormControl isRequired mt={4}>
-            <FormLabel>Farming Type</FormLabel>
-            <Select placeholder="Select option" size="lg">
-              <option value="option1">Livestock</option>
-              <option value="option2">crops</option>
-              <option value="option3">Mixed(crops and animals)</option>
-            </Select>
-          </FormControl>
+            <FormControl mt={4} isInvalid={errors.farmingtype}>
+              <FormLabel>Farming Type</FormLabel>
+              <Select
+                name="farmingtype"
+                ref={register({ validate: validateName })}
+                placeholder="Select option"
+                size="lg"
+              >
+                <option value="livestock">Livestock</option>
+                <option value="crops">crops</option>
+                <option value="mixed">Mixed(crops and animals)</option>
+              </Select>
+              <FormErrorMessage>
+                {errors.farmingtype && errors.farmingtype.message}
+              </FormErrorMessage>
+            </FormControl>
 
-          <FormControl isRequired mt={4}>
-            <FormLabel>Phone Number</FormLabel>
-            <Input width="90%" placeholder="+90348230984" />
-          </FormControl>
+            <FormControl mt={4} isInvalid={errors.phone}>
+              <FormLabel>Phone Number</FormLabel>
+              <Input
+                ref={register({ validate: validateName })}
+                name="phone"
+                width="90%"
+                placeholder="+90348230984"
+              />
+              <FormErrorMessage>
+                {errors.phone && errors.phone.message}
+              </FormErrorMessage>
+            </FormControl>
 
-          <FormControl isRequired mt={4}>
-            <FormLabel>Password</FormLabel>
-            <Input width="90%" placeholder="**********" />
-          </FormControl>
-        </ModalBody>
+            <FormControl mt={4} isInvalid={errors.password}>
+              <FormLabel>Password</FormLabel>
+              <Input
+                name="password"
+                width="90%"
+                ref={register({ validate: validateName })}
+                placeholder="**********"
+              />
+              <FormErrorMessage>
+                {errors.password && errors.password.message}
+              </FormErrorMessage>
+            </FormControl>
+          </ModalBody>
 
-        <ModalFooter alignItems="center">
-          <Button
-            className={styles.navbutton}
-            bg="green.900"
-            variantColor="green"
-            mr={3}
-          >
-            Register
-          </Button>
-          <Button className={styles.navbutton} onClick={closemodal}>
-            Cancel
-          </Button>
-        </ModalFooter>
+          <ModalFooter alignItems="center">
+            <Button
+              className={styles.navbutton}
+              bg="green.900"
+              variantColor="green"
+              mr={3}
+              isDisabled={!formState.dirty}
+              type="submit"
+            >
+              Register
+            </Button>
+            <Button className={styles.navbutton} onClick={closemodal}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );

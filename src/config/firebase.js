28469ee9e6d47b3916ee39accfbf6fa9985fcf4c
ojknation <ojknation@gmail.com>
2020-06-06 18:11:07 +0100

    ft: start integrating with firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,23 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/7.15.0/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/7.15.0/firebase-analytics.js"></script>
+
+// <script>
+//   // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyDZ2-Os1MoMLZszzyHI-nDCbOYfHdrz0Js",
+//     authDomain: "farmapp-dulph.firebaseapp.com",
+//     databaseURL: "https://farmapp-dulph.firebaseio.com",
+//     projectId: "farmapp-dulph",
+//     storageBucket: "farmapp-dulph.appspot.com",
+//     messagingSenderId: "433306739821",
+//     appId: "1:433306739821:web:87b3967894f008641e4b64",
+//     measurementId: "G-Z0BK8LZSNK"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+// </script>
